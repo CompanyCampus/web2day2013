3a244e1b26838e5b37d01ab77b3d04a5e655a556
Clement Delafargue <clement@delafargue.name>
2013-02-02 00:04:23 +0100

    Add: correctly handle CSS and Less files

--- site.hs
+++ site.hs
@@ -12,22 +12,19 @@ main = hakyll $ do
         route   idRoute
         compile copyFileCompiler
 
-    match "css/*" $ do
-        route   idRoute
-        compile compressCssCompiler
     match "font/*" $ do
         route   idRoute
         compile copyFileCompiler
 
---    match "css/*" $ do
---        route   idRoute
---        compile compressCssCompiler
+    match "css/*.css" $ do
+        route   idRoute
+        compile compressCssCompiler
+
     match "css/*.less" $ do
        route   $ setExtension "css"
        compile $ getResourceString >>=
-           withItemBody (unixFilter "./matewrapper.sh" []) >>=
+           withItemBody (unixFilter "lessc" ["-"]) >>=
            return . fmap compressCss
-
 
     match (fromList ["about.rst", "contact.markdown"]) $ do
         route   $ setExtension "html"
