0a131355673437133338286e638c87becaf21c0c
Clement Delafargue <clement@delafargue.name>
2013-02-02 15:54:24 +0100

    Massive refactoring

---
+++ en/blocs/share.md
@@ -0,0 +1,6 @@
+---
+extra_class: 
+bloc_id: share_bloc
+---
+
+<a href="https://twitter.com/share" class="twitter-share-button" data-lang="en">Tweet</a>
--- site.hs
+++ site.hs
@@ -52,13 +52,9 @@ main = hakyll $ do
 -- Events
 --
 
-    forM_ ["en", "fr"] $ \lang ->
-        match (fromGlob $ lang ++ "/events/*.md") $ do
-            route $ setExtension "html"
-            compile $ pandocCompiler
-                >>= loadAndApplyTemplate "templates/event.html"   (globalContext lang)
-                >>= loadAndApplyTemplate "templates/default.html" (globalContext lang)
-                >>= relativizeUrls
+    forM_ ["en", "fr"] $ \lang -> (makeElements lang "events" "event")
+
+    forM_ ["en", "fr"] $ \lang -> (makeIndexPage lang "events" "event")
 
 --------------------------------------------------------------------------------
 -- Speakers
@@ -68,9 +64,11 @@ main = hakyll $ do
         match (fromGlob $ lang ++ "/speakers/*.md") $ do
             route $ setExtension "html"
             compile $ pandocCompiler
-                >>= loadAndApplyTemplate "templates/speaker.html"   (globalContext lang)
+                >>= loadAndApplyTemplate "templates/speaker.html" (globalContext lang)
                 >>= loadAndApplyTemplate "templates/default.html" (globalContext lang)
                 >>= relativizeUrls
+
+    forM_ ["en", "fr"] $ \lang -> (makeIndexPage lang "speakers" "speaker")
 
 --------------------------------------------------------------------------------
 -- Posts
@@ -83,6 +81,8 @@ main = hakyll $ do
                 >>= loadAndApplyTemplate "templates/post.html"   (globalContext lang)
                 >>= loadAndApplyTemplate "templates/default.html" (globalContext lang)
                 >>= relativizeUrls
+
+    forM_ ["en", "fr"] $ \lang -> (makeIndexPage lang "posts" "post")
 
 --------------------------------------------------------------------------------
 -- Index
@@ -120,3 +120,33 @@ blockLoader lang =
     functionField "block" (\args item -> getBlock lang args mempty)
 
 globalContext lang = blockLoader lang `mappend` defaultContext
+
+elementList :: String -> String -> String -> Compiler String
+elementList lang plural singular = do
+    elts <- loadAll $ fromGlob (lang ++ "/" ++ plural ++ "/*.md")
+    tpl  <- loadBody $ fromFilePath ("templates/" ++ singular ++ "-item.html")
+    list <- applyTemplateList tpl defaultContext elts
+    return list
+
+makeIndexPage lang plural singular =
+    create [fromFilePath (lang ++ "/"++ plural ++".html")] $ do
+        route $ setExtension "html"
+        compile $ do
+            let elts =
+                    field plural (\_ -> elementList lang plural singular) `mappend`
+                    globalContext lang
+                tplName = fromFilePath ("templates/" ++ plural ++ "-page.html")
+
+            makeItem ""
+                >>= loadAndApplyTemplate tplName elts
+                >>= loadAndApplyTemplate "templates/default.html" (globalContext lang)
+                >>= relativizeUrls
+
+makeElements lang plural singular =
+    match (fromGlob $ lang ++ "/"++ plural ++"/*.md") $ do
+        route $ setExtension "html"
+        compile $ pandocCompiler
+            >>= loadAndApplyTemplate (
+                fromFilePath $ "templates/"++ singular ++".html")  (globalContext lang)
+            >>= loadAndApplyTemplate "templates/default.html" (globalContext lang)
+            >>= relativizeUrls
